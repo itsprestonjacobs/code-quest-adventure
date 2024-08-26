c5485f1e54c6be895aceebad3e2d81ea99a4e182
itsprestonjacobs <143894974+itsprestonjacobs@users.noreply.github.com>
2024-08-26 17:20:09 -0500

    Update game_data.py

--- game_data.py
+++ game_data.py
@@ -1,1 +1,11 @@
 
+# game_data.py
+
+challenges = [
+    {
+        "question": "Print 'Hello, World!'",
+        "solution": "print('Hello, World!')"
+    },
+    {
+        "question": "Create a function that adds two numbers",
+        "solution": """
